dfa350f89fb4f057e6a7a1a676c063324c46e803
harish <harishg.hiriyur@gmail.com>
2023-11-07 14:58:56 -0500

    update email

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,7 @@
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import React, { useRef, useState } from "react";
-import { FiPhone } from "react-icons/fi/index.esm";
-import { GoLocation, GoMail } from "react-icons/go/index.esm";
+
 const ContactPage = () => {
   const form = useRef();
   const [messageSent, setMessageSent] = useState(false);
@@ -47,23 +46,16 @@ const ContactPage = () => {
 
   return (
     <>
-      <div
-        className="bg-teal-800 text-white pt-10 "
-        // style={{
-        //   backgroundImage: `url(${bgCover_1})`,
-        //   backgroundSize: "cover",
-        // }}
-      >
-        <div className=" mx-auto text-center flex flex-col md:flex-row  justify-end align-center px-4 md:px-10 h-screen">
-          <span className="max-w-[800px] md:w-[800px] flex flex-col  justify-top  pb-10 pt-20 sm:pb-0">
-            <p className="text-[#00df9a] font-bold p-2 sm:text-left text-3xl flex  justify-center sm:justify-start">
+      <div className="bg-teal-800 text-white pt-10">
+        <div className="mx-auto text-center flex flex-col md:flex-row  align-center p-4 md:p-10">
+          <div className="max-w-[600px] md:w-full flex flex-col justify-center pb-10 pt-10 sm:pb-0">
+            <p className="text-[#00df9a] font-bold p-2 sm:text-left text-3xl flex justify-center sm:justify-start">
               Contact Details
             </p>
-            <div className="flex flex-col  ">
+            <div className="flex flex-col">
               <div className="flex justify-center sm:justify-start sm:py-3">
-                <GoLocation size={25} />{" "}
                 <div className="flex flex-col">
-                  <p className="text-white font-bold px-2 sm:text-left text-2xl underline decoration-orange-400">
+                  <p className="text-white font-bold px-2 sm:text-left text-1xl underline decoration-orange-400">
                     Registered offices
                   </p>
                   <h4 className="sm:text-left p-2">
@@ -75,11 +67,9 @@ const ContactPage = () => {
                   </h4>
                 </div>
               </div>
-
-              <div className="flex   justify-center sm:justify-start  sm:py-3">
-                <GoMail size={25} />{" "}
+              <div className="flex justify-center sm:justify-start sm:py-3">
                 <div className="flex flex-col">
-                  <p className="text-white font-bold px-2 sm:text-left text-2xl underline decoration-orange-400">
+                  <p className="text-white font-bold px-2 sm:text-left text-1xl underline decoration-orange-400">
                     Mail us
                   </p>
                   <h4 className="sm:text-left p-2">
@@ -88,87 +78,83 @@ const ContactPage = () => {
                   </h4>
                 </div>
               </div>
-
-              <div className="flex   justify-center sm:justify-start sm:py-3">
-                <FiPhone size={25} />{" "}
+              <div className="flex justify-center sm:justify-start sm:py-3">
                 <div className="flex flex-col">
-                  <p className="text-white font-bold px-2 sm:text-left text-2xl underline decoration-orange-400">
+                  <p className="text-white font-bold px-2 sm:text-left text-1xl underline decoration-orange-400">
                     Call us
                   </p>
                   <h4 className="sm:text-left p-2"> +917676733634</h4>
                 </div>
               </div>
             </div>
-          </span>
+          </div>
 
-          <span className="max-w-[800px] md:w-[800px] flex flex-col justify-top p-20">
-            <div className="w-full md:w-1/2 pb-20 sm:pb-0 sm:pl-20">
-              <h1 className="text-3xl font-bold mb-4 sm:text-left text-[#00df9a]">
-                ENQUIRY FORM
-              </h1>
+          <div className="max-w-[600px] md:w-full flex flex-col justify-top p-4 sm:p-20">
+            <h1 className="text-3xl font-bold mb-4 sm:text-left text-[#00df9a]">
+              ENQUIRY FORM
+            </h1>
 
-              <form ref={form} onSubmit={sendEmail} className="space-y-4 ">
-                <div className="sm:w-[600px]">
-                  <input
-                    required
-                    name="user_name"
-                    className="p-3 flex w-full rounded-md text-black"
-                    type="text"
-                    placeholder="Enter your Name"
-                  />
+            <form ref={form} onSubmit={sendEmail} className="space-y-4">
+              <div className="sm:w-[600px]">
+                <input
+                  required
+                  name="user_name"
+                  className="p-3 flex w-full rounded-md text-black"
+                  type="text"
+                  placeholder="Enter your Name"
+                />
+              </div>
+              <div className="sm:w-[600px]">
+                <input
+                  required
+                  name="user_phoneNumber"
+                  className="p-3 flex w-full rounded-md text-black"
+                  type="number"
+                  placeholder="Phone Number"
+                />
+              </div>
+              <div className="sm:w-[600px]">
+                <input
+                  name="user_email"
+                  className="p-3 flex w-full rounded-md text-black"
+                  type="email"
+                  placeholder="Enter Email"
+                />
+              </div>
+              <div className="sm:w-[600px]">
+                <textarea
+                  required
+                  className="w-full border p-2 rounded-md text-black"
+                  rows="6"
+                  placeholder="Send us a message"
+                  name="message"
+                ></textarea>
+              </div>
+              <div className="flex justify-center sm:justify-start ">
+                <motion.button
+                  whileHover={{ scale: 1.2 }}
+                  onHoverStart={(e) => {}}
+                  onHoverEnd={(e) => {}}
+                  whileTap={{ scale: 0.9 }}
+                  className={`bg-[#00df9a] text-black py-2 px-4 rounded-md font-medium ${
+                    messageSent ? "animate-pulse" : ""
+                  }`}
+                  type="submit"
+                  value="Send"
+                  disabled={messageSent}
+                >
+                  {messageSent ? "Message Sent" : "Send Message"}
+                </motion.button>
+              </div>
+            </form>
+            {messageSent && (
+              <div>
+                <div className="bg-green-500 text-white font-bold rounded-md px-4 py-4">
+                  Thank you, message sent
                 </div>
-                <div className="sm:w-[600px]">
-                  <input
-                    required
-                    name="user_phoneNumber"
-                    className="p-3 flex w-full rounded-md text-black"
-                    type="number"
-                    placeholder="Phone Number"
-                  />
-                </div>
-                <div className="sm:w-[600px]">
-                  <input
-                    name="user_email"
-                    className="p-3 flex w-full rounded-md text-black"
-                    type="email"
-                    placeholder="Enter Email"
-                  />
-                </div>
-                <div className="sm:w-[600px]">
-                  <textarea
-                    required
-                    className="w-full border p-2 rounded-md text-black"
-                    rows="6"
-                    placeholder="Send us a message"
-                    name="message"
-                  ></textarea>
-                </div>
-                <div className="flex justify-center sm:justify-start ">
-                  <motion.button
-                    whileHover={{ scale: 1.2 }}
-                    onHoverStart={(e) => {}}
-                    onHoverEnd={(e) => {}}
-                    whileTap={{ scale: 0.9 }}
-                    className={`bg-[#00df9a] text-black py-2 px-4 rounded-md font-medium ${
-                      messageSent ? "animate-pulse" : ""
-                    }`}
-                    type="submit"
-                    value="Send"
-                    disabled={messageSent}
-                  >
-                    {messageSent ? "Message Sent" : "Send Message"}
-                  </motion.button>
-                </div>
-              </form>
-              {messageSent && (
-                <div>
-                  <div className="bg-green-500 text-white font-bold rounded-md px-4 py-4">
-                    Thank you, message sent
-                  </div>
-                </div>
-              )}
-            </div>
-          </span>
+              </div>
+            )}
+          </div>
         </div>
         <div>
           <iframe
